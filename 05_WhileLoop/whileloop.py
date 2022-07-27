e774da540546b800d5fc42efbf7610c52086b0af
Hi there :) Here is Aarif <105107942+MArifBrohi@users.noreply.github.com>
2022-07-27 19:47:20 +0500

    Create whileloop.py

---
+++ 05_WhileLoop/whileloop.py
@@ -0,0 +1,8 @@
+name = ""
+
+while len(name) == 0:
+    name = input("Enter Your Name: ")
+    #print("Hello "+name ) if you don't input your name, it will output Hello so better for us to
+    # print in next line to terminnate the while loop condition
+print(f"Hello {name}")   
+
