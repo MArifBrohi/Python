c8760783099f96a6107f60d14e45565551c210cd
Hi there :) Here is Aarif <105107942+MArifBrohi@users.noreply.github.com>
2022-07-27 19:46:24 +0500

    Create main.py

---
+++ 04_UserInput/main.py
@@ -0,0 +1,17 @@
+name = input("Enter your name: ")
+first = input("Enter first number ")
+send = input("Enter second number")
+print(f"Your name is {name}")
+
+print(f"Sum of {first} and {send} is {int(first)+int(send)}")
+
+age = int(input("Enter your age: "))
+
+
+if not age>=18: # simply you can write if age>=18:
+                                            # print("YOu are an adult")
+   
+      print("YOu are a child")
+else:
+     print("YOu are an adult")
+
