5f330f3b6d709c3dd5895f4d52f46ac9d9b72db1
Hi there :) Here is Aarif <105107942+MArifBrohi@users.noreply.github.com>
2022-07-27 19:44:17 +0500

    Create main.py

---
+++ 02_Strings/main.py
@@ -0,0 +1,15 @@
+first_name  = "Muhammad"
+last_name = "Arif"
+full_name =  first_name + last_name
+print("hello "+full_name)
+#checking data type
+print(type(last_name))
+
+age  = 21
+age = age+1
+print("Your age is : "+str(age))
+print(type(age))
+
+#multiple assignments
+name1 , age1 = "Jaweria" , 17
+print(str(name1)+" "+str(age1))
