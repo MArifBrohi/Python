3a29593a17d8f0e3692c10947c44d51a98d6ee1d
Hi there :) Here is Aarif <105107942+MArifBrohi@users.noreply.github.com>
2022-07-27 19:49:53 +0500

    Create forloop.py

---
+++ 06_ForLoopWithSound/forloop.py
@@ -0,0 +1,37 @@
+
+from gtts import gTTS
+from playsound import playsound
+import time
+
+#==================================
+# import time
+# for i in range(10):
+#     print(i+1)
+#===================================
+# for i in range(20 , 30+1):
+#     print(i)
+#===================================
+
+# for i in 'Muhammad':
+#     if i == 'u':
+#          print(i)
+#     else:
+#         print(i)
+#===================================
+
+for seconds in range(4):
+   
+     print(seconds)
+     
+     time.sleep(1)
+
+    
+playsound("Sample.mp3")
+print("WE ARE THE FUTURE")
+  
+    
+#     time.sleep(1)
+# def textAudio(text):
+#     audio = gTTS(text)
+#     audio.save("Sample.mp3")
+# textAudio("We welcome you in the world of Artificial Intelligence")
