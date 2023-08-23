bdfb5e82d0576249a5b163d1735e15251a01e9b6
Hi there :) Here is Aarif <105107942+MArifBrohi@users.noreply.github.com>
2023-08-23 11:57:53 +0500

    Add files via upload

---
+++ PYTHON(new codes)/Dictionaries.py
@@ -0,0 +1,11 @@
+marks = {"arif":99,"arshad":22,"shahjahan":12,"daud":45}
+# print(marks.get("arif brohi"))
+# print(marks)
+# print(marks.keys())
+# print(marks.values())
+# print(marks.items())
+
+key = marks.keys()
+
+for val in key:
+    print(val)
